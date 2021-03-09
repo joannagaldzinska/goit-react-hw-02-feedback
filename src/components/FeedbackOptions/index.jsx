43647e7c7c783c5e5f96281ffc0d2be1242a2a2e
Joanna Galdzinska <joanna.galdzinska@gmail.com>
2021-03-09 18:12:04 +0200

    prop types added

--- src/components/FeedbackOptions/index.jsx
+++ src/components/FeedbackOptions/index.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Button = ({ feedback, onLeaveFeedback }) => {
   return (
     <button type="button" data-feedback={feedback} onClick={onLeaveFeedback}>
@@ -11,6 +13,11 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return options.map(option => (
     <Button feedback={option} onLeaveFeedback={onLeaveFeedback} />
   ));
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string),
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
 
 export default FeedbackOptions;
